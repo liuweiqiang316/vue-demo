8b51c9ac8d432413317001adc5200b6ddc5f0fa9
liuweiqiang <liuweiqiang316@163.com>
2023-07-21 00:23:30 +0800

    feat: 4.8 计算属性computed与lazy

--- src/main.js
+++ src/main.js
@@ -21,13 +21,17 @@ export const effect = (fn, options = {}) => {
     cleanup(effectFn);
     activeEffect = effectFn;
     effectStack.push(effectFn);
-    fn();
+    const res = fn();
     effectStack.pop();
     activeEffect = effectStack[effectStack.length - 1];
+    return res;
   };
   effectFn.options = options;
   effectFn.deps = [];
-  effectFn();
+  if (!options.lazy) {
+    effectFn();
+  }
+  return effectFn;
 };
 
 export const track = (target, key) => {
@@ -69,4 +73,31 @@ export const trigger = (target, key) => {
       effectFn();
     }
   });
+};
+
+export const computed = (getter) => {
+  let value;
+  let dirty = true;
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      if (!dirty) {
+        dirty = true;
+        trigger(obj, "value");
+      }
+    },
+  });
+
+  const obj = {
+    get value() {
+      if (dirty) {
+        value = effectFn();
+        dirty = false;
+      }
+      track(obj, "value");
+      return value;
+    },
+  };
+
+  return obj;
 };
