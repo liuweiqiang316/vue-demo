19b3b70025298aafab2661a3cacfcb1cd46b7124
liuweiqiang <liuweiqiang316@163.com>
2023-07-23 11:58:18 +0800

    feat: 4.11 过期的副作用

--- src/main.js
+++ src/main.js
@@ -119,10 +119,17 @@ export const watch = (source, cb, options = {}) => {
   }
 
   let oldValue, newValue;
+  let cleanup;
+  const onInvalidate = (fn) => {
+    cleanup = fn;
+  };
 
   const job = () => {
     newValue = effectFn();
-    cb(newValue, oldValue);
+    if (cleanup) {
+      cleanup();
+    }
+    cb(newValue, oldValue, onInvalidate);
     oldValue = newValue;
   };
 
